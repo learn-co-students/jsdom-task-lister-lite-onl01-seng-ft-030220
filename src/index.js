15612d19961bf034b97aea1258986db56967bc45
pharia-le <pharia.le@gmail.com>
2020-06-10 17:30:18 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    const newtask = document.getElementById("new-task-description").value
+    const tasks = document.getElementById("tasks")
+    const liss = document.createElement("li")
+    liss.innerHTML = newtask
+    tasks.appendChild(liss)
+  });
 });
