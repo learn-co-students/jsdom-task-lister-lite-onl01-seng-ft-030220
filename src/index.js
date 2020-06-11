7b043c48a4e2289df40fc086fd19c509db4bd8f9
code404it <jessikahd.smith@gmail.com>
2020-06-10 20:35:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code heredocument.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("create-task-form").addEventListener("submit", function(event) {
+    event.preventDefault()
+    const newTask = document.getElementById("new-task-description").value
+    const task = document.getElementById("tasks")
+    const list = document.createElement("li")
+    list.innerHTML = newTask
+    task.appendChild(list)
+  })
+})
 });
